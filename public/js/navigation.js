38a2f4a2b5e39e969ec40da84eb3ba752b66996a
ridhoakbar8977 <ridho8977@gmail.com>
2023-03-23 01:08:32 +0700

    tombol non/aktif shortcut keyboard berjalan

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -13,19 +13,33 @@ list.forEach((item) => {
    item.addEventListener("click", activeLink);
 });
 
+var shortcutEnabled = false;
+
+document.querySelector(".power").addEventListener("click", function () {
+   shortcutEnabled = !shortcutEnabled;
+
+   if (shortcutEnabled) {
+      document.body.classList.add("shortcut-enabled");
+   } else {
+      document.body.classList.remove("shortcut-enabled");
+   }
+});
+
+// document.addEventListener("keydown", function (event) {
+//    if (shortcut && event.key === "1") {
+//       prompt("tombol di tekan");
+//    }
+// });
+
+// semua tag a dibuat array agar bisa melooping nya
+const href = ["#home", "#about-me", "#skills", "#portfolio", "#contact-me"];
+
 // menjalankan peristiwa klik dengan keyboard
-const shortcut = () => {
-   document.addEventListener("keydown", function (event) {
-      // semua tag a dibuat array agar bisa melooping nya
-      const href = ["#home", "#about-me", "#skills", "#portfolio", "#contact-me"];
-
-      for (let i = 1; i <= href.length; i++) {
-         // Digit1 = key 1, Digit2 = key2, dst...
-         if (event.code === `Digit${i}`) {
-            document.querySelector(`a[href='${href[i - 1]}']`).click();
-         }
+document.addEventListener("keydown", function (event) {
+   for (let i = 1; i <= href.length; i++) {
+      // Digit1 = key 1, Digit2 = key2, dst...
+      if (shortcutEnabled && event.code === `Digit${i}`) {
+         document.querySelector(`a[href='${href[i - 1]}']`).click();
       }
-   });
-};
-
-shortcut();
+   }
+});
