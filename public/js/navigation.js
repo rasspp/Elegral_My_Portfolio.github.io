4718ca8554316846f7ff660c791048c35786d34a
ridhoakbar8977 <ridho8977@gmail.com>
2023-03-22 18:09:25 +0700

    membuat shortcut keyboard untuk navbar

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,11 +1,31 @@
 const list = document.querySelectorAll(".list");
 
+// membuat peristiwa klik
 function activeLink() {
    list.forEach((item) => {
       item.classList.remove("active");
    });
    this.classList.add("active");
 }
+
+// menjalankan peristiwa klik dengan fungsi klik
 list.forEach((item) => {
    item.addEventListener("click", activeLink);
 });
+
+// menjalankan peristiwa klik dengan keyboard
+const shortcut = () => {
+   document.addEventListener("keydown", function (event) {
+      // semua tag a dibuat array agar bisa melooping nya
+      const href = ["#home", "#about-me", "#skills", "#portfolio", "#contact-me"];
+
+      for (let i = 1; i <= href.length; i++) {
+         // Digit1 = key 1, Digit2 = key2, dst...
+         if (event.code === `Digit${i}`) {
+            document.querySelector(`a[href='${href[i - 1]}']`).click();
+         }
+      }
+   });
+};
+
+shortcut();
