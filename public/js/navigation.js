006d11f651ef435ceb49a225473f8ecf793feec4
ridhoakbar8977 <ridho8977@gmail.com>
2023-03-23 09:35:49 +0700

    developer mode

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -38,7 +38,7 @@ const href = ["#home", "#about-me", "#skills", "#portfolio", "#contact-me"];
 document.addEventListener("keydown", function (event) {
    for (let i = 1; i <= href.length; i++) {
       // Digit1 = key 1, Digit2 = key2, dst...
-      if (shortcutEnabled && event.code === `Digit${i}`) {
+      if (/* shortcutEnabled && */ event.code === `Digit${i}`) {
          document.querySelector(`a[href='${href[i - 1]}']`).click();
       }
    }
